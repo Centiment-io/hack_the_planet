d9753f7fd575f88b07fac9ab957f81744bbf7ada
nmusgrave <naomi.g.musgrave@gmail.com>
2015-08-15 12:28:03 -0700

    starting csv processing function

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,5 +1,15 @@
 // create the controller and inject Angular's $scope
 seedApp.controller('mainController', function($scope) {
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how aasdfasfsasdfasdf I look!';
+        $scope.message = 'Starter page!';
+
+        /*
+        $scope.concentration;
+        $scope.mellow;
+
+        var processEEG(csv) {
+        	var columns;
+        	var lines = csv.split(/\r\n|\n/);
+        }
+        */
 });
