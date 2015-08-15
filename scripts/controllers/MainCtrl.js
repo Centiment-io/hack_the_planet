7da80a10ae9d62148399f3b363d5dc921057c53f
poojasethi <sethi.pooja@gmail.com>
2015-08-15 16:07:45 -0700

    Added helper to parse csv file with eeg data

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,15 +1,23 @@
 // create the controller and inject Angular's $scope
 seedApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Starter page!';
+    // create a message to display in our view
+    $scope.message = 'Starter page!';
 
-        /*
-        $scope.concentration;
-        $scope.mellow;
+    $scope.concentration = processEEG("");
+    $scope.mellow;
 
-        var processEEG(csv) {
-        	var columns;
-        	var lines = csv.split(/\r\n|\n/);
-        }
-        */
+    /** Parses a CSV file of EEG readings and computes the average reading */
+    function processEEG(csv) {
+    	var readingTotal = 0;
+
+    	var readingsList = csv.split("\n");
+    	for (var i = 0; i < readingsList.length; i++) {
+    		var line = readingsList[i].split(","); 
+    		var reading = line[3];
+
+    		readingTotal += reading;
+    	}
+
+    	return readingTotal / readingsList.length();
+    }    
 });
