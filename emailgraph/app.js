cc6712fe02a8c810912e4cbd92b3a00119664ad7
Sam <sam@schakr.com>
2015-08-15 20:47:52 -0700

    Set up central node app

--- emailgraph/app.js
+++ emailgraph/app.js
@@ -1,1 +1,24 @@
-test
+var express = require('express');
+var mongoose 
+
+//starting code
+
+//Check for emails and then populates email_detailed
+var ML = require("./MachineLearning.js");
+ML.start(function(){
+	//Now, we will analyze data and write to JSON file.
+	var calldb = require("./calldb.js");
+	calldb.start(function(){
+
+		console.log("Finished, refresh the graph!");
+
+
+	});
+
+
+	//After analyzing data, refresh the graph?
+
+
+
+});
+
--- emailgraph/calldb.js
+++ emailgraph/calldb.js
@@ -1,9 +1,14 @@
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 
-var url = 'mongodb://52.6.238.55:27017/CONNECTIONS';
-mongoose.connect(url);
-call=function(){}
-baton=[mongoose];
-var firstpart = require('./db1');
-firstpart.start(baton,call);
+exports.start(callback){
+	var url = 'mongodb://52.6.238.55:27017/CONNECTIONS';
+	mongoose.connect(url);
+
+	baton=[mongoose];
+	var firstpart = require('./db1');
+	firstpart.start(baton,callback);
+
+
+}
+
