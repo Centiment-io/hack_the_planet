802efe82bface77dfcd4e6c3f3f58c2305aad8d7
Ubuntu <ubuntu@ip-172-31-10-27.ec2.internal>
2015-08-17 05:57:17 +0000

    final changes on aws before shutting server

--- emailgraph/db2.js
+++ emailgraph/db2.js
@@ -10,15 +10,17 @@ exports.start = function(baton,callback){
     var userSchema = new Schema ({num: Number, fullname: String, email: String,pic:String},{collection: 'userdata'});
     var User = mongoose.model('Userdata', userSchema);
     var edges=[];
-    console.log(ranks);
+    //console.log(ranks);
     User.find({}, function(err, users) {
         if (err){
             console.log(err);
         }   
         var n=users.length;
         var nodesize=[];
+	var connections=[];
         for (i=0;i<n;i++){
             nodesize.push(0);
+	    connections.push([]);
         }
         //console.log(n);
         for (i=0;i<n;i++){
@@ -31,10 +33,34 @@ exports.start = function(baton,callback){
                         edges.push([users[i].num,users[j].num,10.0+4.0/(average+0.01)]);
                         nodesize[users[i].num]+=1;
                         nodesize[users[j].num]+=1;
+			connections[users[i].num].push(users[j].num);
+			connections[users[j].num].push(users[i].num);
                     }
                 }
             }
         }
+	for (i=0;i<5;i++){
+		console.log(nodesize);
+		console.log(connections);
+		var temp=nodesize;
+		for (j=0;j<n;j++){
+			var x=0;
+			for (var k in connections[j]){
+				x+=nodesize[k]/connections[k].length;
+			}
+			temp[j]=x;
+		}
+		var s=0;
+		for (j=0;j<n;j++){
+			s+=temp[j];
+		}
+		for (j=0;j<n;j++){
+			nodesize[j]=temp[j]/s;
+		}
+	}
+	for (j=0;j<n;j++){
+		nodesize[j]*=50;
+	}	
         baton.push(users);
         baton.push(edges);
         baton.push(nodesize);
--- emailgraph/db3.js
+++ emailgraph/db3.js
@@ -7,9 +7,9 @@ exports.start = function(baton,callback){
     var users=baton[3];
     var nodesize=baton[5];
     console.log("third part");
-    console.log(edges);
-    console.log(users);
-    console.log(nodesize);
+    //console.log(edges);
+    //console.log(users);
+    //console.log(nodesize);
     var Schema = mongoose.Schema;
     var tofile2='';
     var tofile1='';
@@ -29,7 +29,7 @@ exports.start = function(baton,callback){
     //console.log(tofile1);
     //console.log(tofile2);
     var text='{"nodes":['+tofile1+'],\n"links":['+tofile2+']}'
-    fs.writeFile("../d3/data.json", text, function(err) {
+    fs.writeFile("./data.json", text, function(err) {
         if(err) {
             return console.log(err);
         }
